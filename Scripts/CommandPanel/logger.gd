3b6e7b489b36bb7b54f7ca6820212d0fa3ee45a2
255-Ping <bchanceyw@gmail.com>
2026-04-12 16:32:08 -0700

    Make logs specific to the second

--- Scripts/CommandPanel/logger.gd
+++ Scripts/CommandPanel/logger.gd
@@ -5,6 +5,12 @@ var save = SaveManager.new()
 var main_panel: Node = null
 var panels: Array
 
+var date_str: String
+
+func _ready() -> void:
+	var date = Time.get_datetime_dict_from_system()
+	date_str = "%d-%02d-%02d %02d:%02d:%02d" % [date.year, date.month, date.day, date.hour, date.minute, date.second]
+
 func log(text: String, panel_node: Node = main_panel):
 	if get_tree():
 		await get_tree().process_frame
@@ -53,8 +59,6 @@ func log_debug(text: String, panel_node: Node = main_panel):
 			panel_loop.log_to_command_panel(str("[color=magenta]Debug: ",text,"[/color]"))
 			
 func _write_to_log(text: String):
-	var date = Time.get_date_dict_from_system()
-	var date_str = "%d-%02d-%02d" % [date.year, date.month, date.day]
 	if !FileAccess.file_exists("user://FlashCards/logs/" + date_str + ".log"):
 		save.create_file("user://FlashCards/logs/" + date_str + ".log", "")
 	var loaded = save.load_text_file("user://FlashCards/logs/" + date_str + ".log")
